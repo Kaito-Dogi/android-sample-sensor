4603341339de56cbb19144467124fc196a46d37d
Kaito Dogi <kendobu0405@gmail.com>
2022-06-02 03:44:23 +0900

    feat: センサーをインスタンス化

--- app/src/main/java/com/doggy/sensorsample/MainActivity.kt
+++ app/src/main/java/com/doggy/sensorsample/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.doggy.sensorsample
 
+import android.content.Context
 import android.hardware.Sensor
 import android.hardware.SensorManager
 import android.os.Bundle
@@ -28,5 +29,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // SensorManagerのインスタンスを生成
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
+        // 加速度センサーを取得する
+        mAccelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+
+        // 地磁気センサーを取得する
+        mMagneticFieldSensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)
     }
 }
