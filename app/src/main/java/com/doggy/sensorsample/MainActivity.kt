52275cddaa09138ff85ff0156f8bdd1e8bd3de99
Kaito Dogi <kendobu0405@gmail.com>
2022-06-02 03:43:22 +0900

    feat: 必要なフィールドを作成

--- app/src/main/java/com/doggy/sensorsample/MainActivity.kt
+++ app/src/main/java/com/doggy/sensorsample/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.doggy.sensorsample
 
+import android.hardware.Sensor
+import android.hardware.SensorManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.doggy.sensorsample.databinding.ActivityMainBinding
@@ -7,6 +9,20 @@ import com.doggy.sensorsample.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    // SensorManager
+    private lateinit var sensorManager: SensorManager
+
+    // Sensor
+    private var mAccelerometerSensor: Sensor? = null
+    private var mMagneticFieldSensor: Sensor? = null
+
+    // Sensorの値
+    private var mAccelerometerValue: FloatArray = FloatArray(3)
+    private var mMagneticFieldValue: FloatArray = FloatArray(5)
+
+    // 一度でも地磁気センサーの値を取得したか
+    private var mMagneticFiledFlg: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
